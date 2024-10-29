e67b1dea35326295b2de15ee677d035504e91fc1
jijiarong <jijiarong@huawei.com>
2024-10-29 11:34:11 +0800

    cmt mint adaptation

--- mindcv/models/cmt.py
+++ mindcv/models/cmt.py
@@ -4,6 +4,7 @@ import mindspore
 import mindspore.common.initializer as init
 import mindspore.nn as nn
 import mindspore.ops as ops
+import mindspore.mint as mint
 
 from .helpers import _ntuple, load_pretrained
 from .layers import Dropout, DropPath, GlobalAvgPooling
@@ -40,39 +41,39 @@ to_2tuple = _ntuple(2)
 
 
 def swish(x):
-    return x * ops.Sigmoid()(x)
+    return x * mint.sigmoid(x)
 
 
 class Mlp(nn.Cell):
-    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=mint.nn.GELU, drop=0.):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
         self.conv1 = nn.SequentialCell([
             nn.Conv2d(in_features, hidden_features, 1, 1, has_bias=True),
-            nn.GELU(),
-            nn.BatchNorm2d(hidden_features),
+            mint.nn.GELU(),
+            mint.nn.BatchNorm2d(hidden_features),
         ])
         self.proj = nn.Conv2d(
             hidden_features, hidden_features, 3, 1, pad_mode='pad', padding=1, group=hidden_features, has_bias=True)
-        self.proj_act = nn.GELU()
-        self.proj_bn = nn.BatchNorm2d(hidden_features)
+        self.proj_act = mint.nn.GELU()
+        self.proj_bn = mint.nn.BatchNorm2d(hidden_features)
         self.conv2 = nn.SequentialCell([
             nn.Conv2d(hidden_features, out_features, 1, 1, has_bias=True),
-            nn.BatchNorm2d(out_features),
+            mint.nn.BatchNorm2d(out_features),
         ])
         self.drop = Dropout(p=drop)
 
     def construct(self, x, H, W):
         B, _, C = x.shape
-        x = ops.transpose(x, (0, 2, 1)).reshape(B, C, H, W)
+        x = mint.permute(x, (0, 2, 1)).reshape(B, C, H, W)
         x = self.conv1(x)
         x = self.drop(x)
         x = self.proj(x) + x
         x = self.proj_act(x)
         x = self.proj_bn(x)
         x = self.conv2(x)
-        x = ops.transpose(x.reshape(B, C, -1), (0, 2, 1))
+        x = mint.permute(x.reshape(B, C, -1), (0, 2, 1))
         x = self.drop(x)
         return x
 
@@ -86,11 +87,11 @@ class Attention(nn.Cell):
         self.scale = qk_scale or head_dim ** -0.5
         self.qk_dim = dim // qk_ratio
 
-        self.q = nn.Dense(dim, self.qk_dim, has_bias=qkv_bias)
-        self.k = nn.Dense(dim, self.qk_dim, has_bias=qkv_bias)
-        self.v = nn.Dense(dim, dim, has_bias=qkv_bias)
+        self.q = mint.nn.Linear(dim, self.qk_dim, bias=qkv_bias)
+        self.k = mint.nn.Linear(dim, self.qk_dim, bias=qkv_bias)
+        self.v = mint.nn.Linear(dim, dim, bias=qkv_bias)
         self.attn_drop = Dropout(p=attn_drop)
-        self.proj = nn.Dense(dim, dim)
+        self.proj = mint.nn.Linear(dim, dim)
         self.proj_drop = Dropout(p=proj_drop)
 
         self.sr_ratio = sr_ratio
@@ -99,40 +100,40 @@ class Attention(nn.Cell):
             self.sr = nn.SequentialCell([
                 nn.Conv2d(dim, dim, kernel_size=sr_ratio,
                           stride=sr_ratio, group=dim, has_bias=True),
-                nn.BatchNorm2d(dim),
+                mint.nn.BatchNorm2d(dim),
             ])
 
-        self.softmax = nn.Softmax(axis=-1)
+        self.softmax = mint.nn.Softmax(dim=-1)
 
     def construct(self, x, H, W, relative_pos):
         B, N, C = x.shape
         q = self.q(x).reshape(B, N, self.num_heads,
                               self.qk_dim // self.num_heads)
-        q = ops.transpose(q, (0, 2, 1, 3))
+        q = mint.permute(q, (0, 2, 1, 3))
 
         if self.sr_ratio > 1:
-            x_ = ops.transpose(x, (0, 2, 1)).reshape(B, C, H, W)
+            x_ = mint.permute(x, (0, 2, 1)).reshape(B, C, H, W)
             x_ = self.sr(x_).reshape(B, C, -1)
-            x_ = ops.transpose(x_, (0, 2, 1))
+            x_ = mint.permute(x_, (0, 2, 1))
             k = self.k(x_).reshape(B, -1, self.num_heads,
                                    self.qk_dim // self.num_heads)
-            k = ops.transpose(k, (0, 2, 1, 3))
+            k = mint.permute(k, (0, 2, 1, 3))
             v = self.v(x_).reshape(B, -1, self.num_heads, C // self.num_heads)
-            v = ops.transpose(v, (0, 2, 1, 3))
+            v = mint.permute(v, (0, 2, 1, 3))
         else:
             k = self.k(x).reshape(B, N, self.num_heads,
                                   self.qk_dim // self.num_heads)
-            k = ops.transpose(k, (0, 2, 1, 3))
+            k = mint.permute(k, (0, 2, 1, 3))
             v = self.v(x).reshape(B, N, self.num_heads, C // self.num_heads)
-            v = ops.transpose(v, (0, 2, 1, 3))
+            v = mint.permute(v, (0, 2, 1, 3))
 
-        attn = mindspore.ops.matmul(q, ops.Transpose()(
+        attn = mint.matmul(q, mint.permute(
             k, (0, 1, 3, 2))) * self.scale + relative_pos
 
         attn = self.softmax(attn)
         attn = self.attn_drop(attn)
-        x = mindspore.ops.matmul(attn, v)
-        x = ops.transpose(x, (0, 2, 1, 3)).reshape(B, N, C)
+        x = mint.matmul(attn, v)
+        x = mint.permute(x, (0, 2, 1, 3)).reshape(B, N, C)
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
@@ -140,13 +141,14 @@ class Attention(nn.Cell):
 
 class Block(nn.Cell):
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, qk_ratio=1, sr_ratio=1):
+                 drop_path=0., act_layer=mint.nn.GELU, norm_layer=mint.nn.LayerNorm, qk_ratio=1, sr_ratio=1):
         super().__init__()
         self.norm1 = norm_layer([dim])
         self.attn = Attention(
             dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
             attn_drop=attn_drop, proj_drop=drop, qk_ratio=qk_ratio, sr_ratio=sr_ratio)
         # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        # TODO: ops.Identity 已收录，不支持
         self.drop_path = DropPath(
             drop_path) if drop_path > 0. else ops.Identity()
         self.norm2 = norm_layer([dim])
@@ -158,9 +160,9 @@ class Block(nn.Cell):
 
     def construct(self, x, H, W, relative_pos):
         B, _, C = x.shape
-        cnn_feat = ops.transpose(x, (0, 2, 1)).reshape(B, C, H, W)
+        cnn_feat = mint.permute(x, (0, 2, 1)).reshape(B, C, H, W)
         x = self.proj(cnn_feat) + cnn_feat
-        x = ops.transpose(x.reshape(B, C, H * W), (0, 2, 1))
+        x = mint.permute(x.reshape(B, C, H * W), (0, 2, 1))
         x = x + self.drop_path(self.attn(self.norm1(x), H, W, relative_pos))
         x = x + self.drop_path(self.mlp(self.norm2(x), H, W))
         return x
@@ -186,7 +188,7 @@ class PatchEmbed(nn.Cell):
 
         self.proj = nn.Conv2d(
             in_chans, embed_dim, kernel_size=patch_size, stride=patch_size, has_bias=True)
-        self.norm = nn.LayerNorm([embed_dim])
+        self.norm = mint.nn.LayerNorm([embed_dim])
 
     def construct(self, x):
         _, _, H, W = x.shape
@@ -195,7 +197,7 @@ class PatchEmbed(nn.Cell):
             f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
         x = self.proj(x)
         _B, _C, _H, _W = x.shape
-        x = ops.transpose(x.reshape(_B, _C, _H * _W), (0, 2, 1))
+        x = mint.permute(x.reshape(_B, _C, _H * _W), (0, 2, 1))
         x = self.norm(x)
 
         H, W = H // self.patch_size[0], W // self.patch_size[1]
@@ -226,22 +228,22 @@ class CMT(nn.Cell):
         super().__init__()
         self.num_classes = num_classes
         self.num_features = self.embed_dim = embed_dims[-1]
-        norm_layer = norm_layer or nn.LayerNorm
+        norm_layer = norm_layer or mint.nn.LayerNorm
 
         self.stem_conv1 = nn.Conv2d(
             3, stem_channel, kernel_size=3, stride=2, pad_mode='pad', padding=1, has_bias=True)
-        self.stem_relu1 = nn.GELU()
-        self.stem_norm1 = nn.BatchNorm2d(stem_channel)
+        self.stem_relu1 = mint.nn.GELU()
+        self.stem_norm1 = mint.nn.BatchNorm2d(stem_channel)
 
         self.stem_conv2 = nn.Conv2d(
             stem_channel, stem_channel, kernel_size=3, stride=1, pad_mode='pad', padding=1, has_bias=True)
-        self.stem_relu2 = nn.GELU()
-        self.stem_norm2 = nn.BatchNorm2d(stem_channel)
+        self.stem_relu2 = mint.nn.GELU()
+        self.stem_norm2 = mint.nn.BatchNorm2d(stem_channel)
 
         self.stem_conv3 = nn.Conv2d(
             stem_channel, stem_channel, kernel_size=3, stride=1, pad_mode='pad', padding=1, has_bias=True)
-        self.stem_relu3 = nn.GELU()
-        self.stem_norm3 = nn.BatchNorm2d(stem_channel)
+        self.stem_relu3 = mint.nn.GELU()
+        self.stem_norm3 = mint.nn.BatchNorm2d(stem_channel)
 
         self.patch_embed_a = PatchEmbed(
             img_size=img_size // 2, patch_size=2, in_chans=stem_channel, embed_dim=embed_dims[0])
@@ -252,22 +254,22 @@ class CMT(nn.Cell):
         self.patch_embed_d = PatchEmbed(
             img_size=img_size // 16, patch_size=2, in_chans=embed_dims[2], embed_dim=embed_dims[3])
 
-        self.relative_pos_a = ops.zeros(
+        self.relative_pos_a = mint.zeros(
             (num_heads[0], self.patch_embed_a.num_patches,
              self.patch_embed_a.num_patches // sr_ratios[0] // sr_ratios[0]),
-            mindspore.float32)
-        self.relative_pos_b = ops.zeros(
+            dtype=mindspore.float32)
+        self.relative_pos_b = mint.zeros(
             (num_heads[1], self.patch_embed_b.num_patches,
              self.patch_embed_b.num_patches // sr_ratios[1] // sr_ratios[1]),
-            mindspore.float32)
-        self.relative_pos_c = ops.zeros(
+            dtype=mindspore.float32)
+        self.relative_pos_c = mint.zeros(
             (num_heads[2], self.patch_embed_c.num_patches,
              self.patch_embed_c.num_patches // sr_ratios[2] // sr_ratios[2]),
-            mindspore.float32)
-        self.relative_pos_d = ops.zeros(
+            dtype=mindspore.float32)
+        self.relative_pos_d = mint.zeros(
             (num_heads[3], self.patch_embed_d.num_patches,
              self.patch_embed_d.num_patches // sr_ratios[3] // sr_ratios[3]),
-            mindspore.float32)
+            dtype=mindspore.float32)
 
         # stochastic depth decay rule
         dpr = [x.item() for x in np.linspace(0, drop_path_rate, sum(depths))]
@@ -307,9 +309,10 @@ class CMT(nn.Cell):
         # Classifier head
         self._fc = nn.Conv2d(
             embed_dims[-1], fc_dim, kernel_size=1, has_bias=True)
-        self._bn = nn.BatchNorm2d(fc_dim)
+        self._bn = mint.nn.BatchNorm2d(fc_dim)
         self._drop = Dropout(p=drop_rate)
-        self.head = nn.Dense(
+        # ops.Identity 已收录，不支持
+        self.head = mint.nn.Linear(
             fc_dim, num_classes) if num_classes > 0 else ops.Identity()
 
         self._initialize_weights()
@@ -324,16 +327,16 @@ class CMT(nn.Cell):
                     cell.bias.set_data(
                         init.initializer('zeros', cell.bias.shape, cell.bias.dtype))
 
-            if isinstance(cell, nn.Dense):
+            if isinstance(cell, mint.nn.Linear):
                 cell.weight.set_data(init.initializer(init.TruncatedNormal(sigma=0.02), cell.weight.shape,
                                                       cell.weight.dtype))
                 if cell.bias is not None:
                     cell.bias.set_data(
                         init.initializer('zeros', cell.bias.shape, cell.bias.dtype))
 
-            elif isinstance(cell, (nn.LayerNorm, nn.BatchNorm2d)):
-                cell.gamma.set_data(init.initializer('ones', cell.gamma.shape, cell.gamma.dtype))
-                cell.beta.set_data(init.initializer('zeros', cell.beta.shape, cell.beta.dtype))
+            elif isinstance(cell, (mint.nn.LayerNorm, mint.nn.BatchNorm2d)):
+                cell.weight.set_data(init.initializer('ones', cell.weight.shape, cell.weight.dtype))
+                cell.bias.set_data(init.initializer('zeros', cell.bias.shape, cell.bias.dtype))
 
     def forward_features(self, x):
         B = x.shape[0]
@@ -353,24 +356,24 @@ class CMT(nn.Cell):
         for _, blk in enumerate(self.blocks_a):
             x = blk(x, H, W, self.relative_pos_a)
 
-        x = ops.transpose(x.reshape(B, H, W, -1), (0, 3, 1, 2))
+        x = mint.permute(x.reshape(B, H, W, -1), (0, 3, 1, 2))
         x, (H, W) = self.patch_embed_b(x)
         for _, blk in enumerate(self.blocks_b):
             x = blk(x, H, W, self.relative_pos_b)
 
-        x = ops.transpose(x.reshape(B, H, W, -1), (0, 3, 1, 2))
+        x = mint.permute(x.reshape(B, H, W, -1), (0, 3, 1, 2))
         x, (H, W) = self.patch_embed_c(x)
         for _, blk in enumerate(self.blocks_c):
             x = blk(x, H, W, self.relative_pos_c)
 
-        x = ops.transpose(x.reshape(B, H, W, -1), (0, 3, 1, 2))
+        x = mint.permute(x.reshape(B, H, W, -1), (0, 3, 1, 2))
         x, (H, W) = self.patch_embed_d(x)
         for _, blk in enumerate(self.blocks_d):
             x = blk(x, H, W, self.relative_pos_d)
 
         B, _, C = x.shape
 
-        x = self._fc(ops.transpose(x, (0, 2, 1)).reshape(B, C, H, W))
+        x = self._fc(mint.permute(x, (0, 2, 1)).reshape(B, C, H, W))
         x = self._bn(x)
         x = swish(x)
         x = GlobalAvgPooling()(x)
